b4b03ed0b42721493b35f84c8ec1520723b08f21
jomefavourite <54733225+jomefavourite@users.noreply.github.com>
2020-08-14 01:55:04 +0100

    code

---
+++ interior-consultant-master/app.js
@@ -0,0 +1,17 @@
+function call(name) {
+  return document.querySelector(name)
+}
+
+let ham = call('.ham'),
+    nav = call('.nav__toggle'),
+    nav2 = call('.nav');
+
+ham.addEventListener('click', () => {
+  document.body.classList.toggle('display-overf')
+  ham.classList.toggle('change')
+  nav2.classList.toggle('display')
+
+})
+
+
+
